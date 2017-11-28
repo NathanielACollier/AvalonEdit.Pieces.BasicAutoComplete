e2e83c696f1f40ffb211f67f82ea61d67d6c9996
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-11-28 11:24:30 -0600

    Put the generic control adorner back
    We use it in the unit tests

---
+++ AvalonEdit.Pieces/GenericControlAdorner.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace AvalonEdit.Pieces
+{
+    // original from: http://www.nbdtech.com/Blog/archive/2010/06/28/wpf-adorners-part-2-ndash-placing-any-control-on-the.aspx
+    public class GenericControlAdorner : Adorner
+    {
+        private Control _child;
+
+        public GenericControlAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index != 0) throw new ArgumentOutOfRangeException();
+            return _child;
+        }
+
+        public Control Child
+        {
+            get { return _child; }
+            set
+            {
+                if (_child != null)
+                {
+                    RemoveVisualChild(_child);
+                }
+                _child = value;
+                if (_child != null)
+                {
+                    AddVisualChild(_child);
+                }
+            }
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            _child.Measure(constraint);
+            return _child.DesiredSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _child.Arrange(new Rect(new Point(0, 0), finalSize));
+            return new Size(_child.ActualWidth, _child.ActualHeight);
+        }
+
+
+    }
+}
